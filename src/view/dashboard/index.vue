f6e4364ccf5d470eacb81722cacf719bb4a4de23
xiaolin <462441355@qq.com>
2018-11-24 14:26:18 +0800

    * 删除不必要的代码   减小打包

--- src/view/dashboard/index.vue
+++ src/view/dashboard/index.vue
@@ -1,19 +1,15 @@
 <template>
-	<div class="dashboard-editor-container">
-    
+	<div class="app-container">
+    泰森后台
   </div>
 </template>
 
 <script>
-import PanThumb from 'base/PanThumb'
-import Github from 'base/Github'
 
 export default {
   name: 'dashboard-editor',
-  components: { PanThumb, Github},
   data () {
     return {
-      avatar: 'http://oni42o7kl.bkt.clouddn.com/tx.gif'
     }
   },
   computed: {
@@ -22,35 +18,5 @@ export default {
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-.emptyGif {
-  display: block;
-  width: 45%;
-  margin: 0 auto;
-}
 
-.dashboard-editor-container {
-  background-color: #e3e3e3;
-  min-height: 100vh;
-  margin-top: -50px;
-  padding: 100px 60px 0px;
-  .pan-info-roles {
-    font-size: 12px;
-    font-weight: 700;
-    color: #333;
-    display: block;
-  }
-  .info-container {
-    position: relative;
-    margin-left: 190px;
-    height: 150px;
-    line-height: 200px;
-    .display_name {
-      font-size: 48px;
-      line-height: 48px;
-      color: #212121;
-      position: absolute;
-      top: 25px;
-    }
-  }
-}
 </style>
