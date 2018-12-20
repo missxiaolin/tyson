0d735e3c4ba8568fa44f8372458c4a275afdc855
xiaolin <462441355@qq.com>
2018-12-20 11:19:43 +0800

    * add

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // BASE_API: '"http://www.tyson.com.cn:9999"'
-  BASE_API: '"http://39.105.203.57:9999"'
+  BASE_API: '"http://www.tyson.com.cn:9999"'
+  // BASE_API: '"http://39.105.203.57:9999"'
 }
